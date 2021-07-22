12fd2d5ef32828c07734d171c720fb48ebefc58f
Matheus Passos <matheushtip@gmail.com>
2021-07-22 14:56:59 -0300

    :sparkles: Criação do model para videos

--- playlist/admin.py
+++ playlist/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from playlist.models import Video
+
+
+@admin.register(Video)
+class Video(admin.ModelAdmin):
+    list_display = ['id','titulo', 'descricao']
+    list_display_links = ['id', 'titulo']
--- playlist/models.py
+++ playlist/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Video(models.Model):
+    titulo = models.CharField(max_length=200, blank=False, null=False)
+    descricao = models.TextField(max_length=600, blank=False, null=False)
+    url = models.URLField(blank=False, null=False)
+
+    def __str__(self):
+        return self.titulo
+
+    class Meta:
+        verbose_name = 'Video'
+        verbose_name_plural = 'Videos'
