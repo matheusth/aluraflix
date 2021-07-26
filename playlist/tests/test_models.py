10c1cbc19f5c920b901b58dc03f21fc034db1aa0
Matheus Passos <matheushtip@gmail.com>
2021-07-26 11:17:32 -0300

    :white_check_mark: Add testing for the Categoria model.

--- playlist/tests/test_models.py
+++ playlist/tests/test_models.py
@@ -1,5 +1,22 @@
 from django.test import testcases
+from playlist.models import Categoria
+from django.db.utils import IntegrityError
 
 
-class VideoModelTestCase(testcases.TestCase):
-    pass
+class CategoriaModelTestCase(testcases.TestCase):
+    def setUp(self) -> None:
+        self.titulo = "Hello World!"
+        self.cor = 'teal'
+
+    def test_should_save_categoria_if_no_info_is_null(self):
+        Categoria.objects.create(titulo=self.titulo, cor=self.cor)
+        self.assertEqual(Categoria.objects.count(), 1)
+        self.assertEqual(Categoria.objects.last().titulo, self.titulo)
+        self.assertEqual(Categoria.objects.last().cor, self.cor)
+
+    def test_should_failed_to_save_categoria_if_some_info_is_null(self):
+        with self.assertRaises(IntegrityError):
+            Categoria.objects.create(titulo=self.titulo, cor=None)
+
+        with self.assertRaises(IntegrityError):
+            Categoria.objects.create(titulo=None, cor=self.cor)
