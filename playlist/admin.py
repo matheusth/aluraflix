cc3a186ca214fb1a1643d1dada47361183e36f40
Matheus Passos <matheushtip@gmail.com>
2021-07-28 16:34:05 -0300

    :sparkles: add Categoria to the django admin.

--- playlist/admin.py
+++ playlist/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 
-from playlist.models import Video
+from playlist.models import Video, Categoria
 
 
 @admin.register(Video)
-class Video(admin.ModelAdmin):
-    list_display = ['id','titulo', 'descricao']
+class Videos(admin.ModelAdmin):
+    list_display = ['id', 'titulo', 'descricao']
+    list_display_links = ['id', 'titulo']
+
+
+@admin.register(Categoria)
+class Categorias(admin.ModelAdmin):
+    list_display = ['id', 'titulo', 'cor']
     list_display_links = ['id', 'titulo']
